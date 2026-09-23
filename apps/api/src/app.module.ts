19c94a76b405183fcde53ad42ac0ce1e8095b7b4
foxx1 <essa_134@hotmail.com>
2026-09-23 09:09:08 +0300

    feat: register database and matches modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { DatabaseModule } from "./database/database.module";
 import { HealthModule } from "./modules/health/health.module";
+import { MatchesModule } from "./modules/matches/matches.module";
 
 @Module({
-  imports: [HealthModule]
+  imports: [DatabaseModule, HealthModule, MatchesModule]
 })
 export class AppModule {}
