0daba6a9601d76413096be8c54f093343fcb2932
foxx1 <essa_134@hotmail.com>
2026-09-23 10:58:42 +0300

    feat: enforce authentication globally

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,6 +1,10 @@
-import { Global, Module } from "@nestjs/common";
-import { AuthGuard } from "./auth.guard";
+import { Global, Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Global()
-@Module({ providers: [AuthGuard], exports: [AuthGuard] })
+@Module({
+  providers: [AuthGuard, { provide: APP_GUARD, useExisting: AuthGuard }],
+  exports: [AuthGuard]
+})
 export class AuthModule {}
